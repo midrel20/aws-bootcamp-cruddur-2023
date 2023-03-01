14334b3a703b5fe0cfe16701391e44ca639866a6
midrel20 <midrel.temgoua@gmail.com>
2023-03-01 09:48:59 +0000

    correction xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -39,9 +39,12 @@ tracer = trace.get_tracer(__name__)
 #AWS XRAY
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 app = Flask(__name__)
+
+#AWS XRAY
+XRayMiddleware(app, xray_recorder)
 
 #honeycomb deb
 # Initialize automatic instrumentation with Flask
