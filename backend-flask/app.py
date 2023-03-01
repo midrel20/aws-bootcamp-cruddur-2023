25861db3276f84981cfebb68efdaa9e385a0c1c5
midrel20 <midrel.temgoua@gmail.com>
2023-03-01 12:11:58 +0000

    implement cloudwatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,6 +26,20 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+#cloudwatch 
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
+
 
 #honeycomb
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -37,14 +51,14 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 #AWS XRAY
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 app = Flask(__name__)
 
 #AWS XRAY
-XRayMiddleware(app, xray_recorder)
+#XRayMiddleware(app, xray_recorder)
 
 #honeycomb deb
 # Initialize automatic instrumentation with Flask
@@ -63,6 +77,13 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#@app.after_request
+#def after_request(response):
+#    timestamp = strftime('[%Y-%b-%d %H:%M]')
+#    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#    return response
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -101,6 +122,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  #data = HomeActivities.run(Logger=LOGGER)
   data = HomeActivities.run()
   return data, 200
 
